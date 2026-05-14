2731a21d21a49fe5e60650cb6058595a26d2c2bd
Dario Klein <dariokleinalves@gmail.com>
2026-05-14 02:45:24 -0300

    fix leaf not used

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Leaf, Menu, X } from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 import { Link, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import logo from '../../public/favicon.png'
